2a161e016d69f2d178fc7612cd51bbcf71c4a22b
Ayberk Aydin <>
2021-03-14 22:35:37 +0300

    upd

--- src/train.py
+++ src/train.py
@@ -12,7 +12,6 @@ import torchfunc, torchlayers, torchsummary
 
 import numpy as np
 import matplotlib.pyplot as plt
-
 from torchvision import transforms
 
 
@@ -27,42 +26,50 @@ log_path = os.path.join(project_path, "logs")
 ckp_path = os.path.join(project_path, "model_checkpoints")
 data_path = os.path.join(project_path, "data")
 
+from omegaconf import DictConfig, OmegaConf
+import hydra
 
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-from pytorch_lightning.callbacks.gpu_stats_monitor import GPUStatsMonitor
-from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
-from pytorch_lightning.loggers import TensorBoardLogger
-
-
-gpu_stats = GPUStatsMonitor()
-early_stopping = EarlyStopping(
-    monitor="validation_accuracy", patience=3, verbose=True, mode="max"
-)
+@hydra.main(config_name='config')
+def my_app(cfg):
+    from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+    from pytorch_lightning.callbacks.gpu_stats_monitor import GPUStatsMonitor
+    from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+    from pytorch_lightning.loggers import TensorBoardLogger
 
 
-tb_logger = TensorBoardLogger(save_dir=log_path, name="my_MODEL")
-checkpoint = ModelCheckpoint(
-    dirpath=ckp_path,
-    filename="epoch{epoch}model",
-    monitor="validation_accuracy",
-    mode="max",
-)
+    gpu_stats = GPUStatsMonitor()
+
+    early_stopping = EarlyStopping(
+        monitor="validation_accuracy",
+        patience=cfg.hparams.early_stopping_patience,
+        verbose=True, 
+        mode="max"
+    )
+
+    tb_logger = TensorBoardLogger(save_dir=log_path, 
+                                  name="my_MODEL")
+    checkpoint = ModelCheckpoint(
+        dirpath=ckp_path,
+        filename="epoch{epoch}model",
+        monitor="validation_accuracy",
+        mode="max",
+    )
 
 
-trainer = pl.Trainer(
-    # limit_train_batches=0.015,
-    # limit_val_batches=0.02,
-    progress_bar_refresh_rate=200,
-    gpus=1,
-    accelerator="ddp",
-    callbacks=[gpu_stats, early_stopping, checkpoint],
-    logger=tb_logger,
-)
+    trainer = pl.Trainer(
+        # limit_train_batches=0.015,
+        # limit_val_batches=0.02,
+        progress_bar_refresh_rate=50,
+        gpus=1,
+        accelerator="ddp",
+        callbacks=[gpu_stats, early_stopping, checkpoint],
+        logger=tb_logger,
+    )
 
 
-model = LightningModule(model=Resnet50())
-datamodule = ImagenetDataModule(
-    path=os.path.join(data_path, "ILSVRC/Data/CLS-LOC"), batch_size=16
-)
+    model = LightningModule(model=Resnet50())
+    datamodule = ImagenetDataModule(
+        path=os.path.join(data_path, "ILSVRC/Data/CLS-LOC"), batch_size=16
+    )
 
-trainer.fit(model, datamodule)
+    trainer.fit(model, datamodule)
