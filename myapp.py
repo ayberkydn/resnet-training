9f4b2f7239ae7c2635fdbcc9df7ec81d5488436c
Ayberk Aydin <>
2021-03-17 00:38:15 +0300

    upd

--- myapp.py
+++ myapp.py
@@ -5,12 +5,12 @@ import numpy as np
 import os
 
 
-
-@hydra.main(config_name="configs/config.yaml")
+@hydra.main(config_name="configs/config")
 def my_app(cfg: DictConfig) -> None:
 
     print(hydra.utils.get_original_cwd())
     print(os.getcwd())
+
 
 if __name__ == "__main__":
     my_app()
